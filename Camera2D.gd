c21b23998089e7a19342e2dfbff55655d2e33688
jtlung <jtlung01@gmail.com>
2023-11-11 22:58:45 -0500

    Terrain Template
    terrain template

---
+++ Camera2D.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+var player = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta):
+	player = get_node_or_null("/root/Game/Player")
+	if player:
+		global_position = player.global_position
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const maxSpeed = 750.0
+const walkSpeed = 200
+const jumpForce = -400.0
+var currentSpeed = 0.0
+var acceleration = 35
+var deceleration = 15
+var running = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,27 +17,53 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		if not ($Sprite.animation == "Jump" and $Sprite.is_playing()):
-			$Sprite.animation = "Fall"
-	elif abs(velocity.x) > 0:
-		$Sprite.play("Walk")
+		if Input.is_action_pressed("Dash"):
+			velocity.y = clamp(velocity.y,-999999,50)
+		if not ($Sprite.animation == "Jump" and $Sprite.is_playing() and $Sprite.animation != "Fall"):
+			$Sprite.play("Fall")
+	elif abs(velocity.x) > 50:
+		if running:
+			$Sprite.play("Run")
+		else:
+			$Sprite.play("Walk")
 	else:
 		$Sprite.animation = "Idle"
 	# Handle Jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		$Sprite.play("Jump")
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jumpForce
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("Left", "Right")
+	var targetSpeed = walkSpeed
+	if Input.is_action_pressed("Run"):
+		running = true
+		targetSpeed = maxSpeed
+	else:
+		running = false
 	if direction:
-		velocity.x = direction * SPEED
 		if direction < 0:
 			$Sprite.flip_h = true
 		else:
 			$Sprite.flip_h = false
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		if currentSpeed*direction < targetSpeed:
+			if is_on_floor():
+				currentSpeed+= direction*acceleration
+			else:
+				currentSpeed+= direction*acceleration*.5
+	elif abs(currentSpeed) > 0 and is_on_floor():
+		currentSpeed-= deceleration*sign(currentSpeed)
+	if abs(currentSpeed) > targetSpeed:
+		currentSpeed-= deceleration*sign(currentSpeed)
+	if abs(currentSpeed) < 10:
+		currentSpeed = 0
+	currentSpeed = clamp(currentSpeed,-maxSpeed,maxSpeed)
+	var ray = $RayCast2D
+	ray.target_position.x = 22*sign(currentSpeed)
+	if ray.is_colliding():
+		currentSpeed = -currentSpeed/2
+		if abs(currentSpeed*2) < 100:
+			currentSpeed = 0
+	velocity.x = currentSpeed
 	move_and_slide()
